3a8bdad02bc87ba9c635b1542bcd2979d156cec5
andziuba <121431990+andziuba@users.noreply.github.com>
2023-09-13 01:31:34 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import { displayContent, loadDefaultContent } from "./modules/dom";
+import { displayNewCategoryModal, displayNewTaskModal, closeModals } from "./modules/modals";
+import { submitNewCategory } from "./modules/category";
+import { submitNewTask } from "./modules/task";
+
+displayContent("All tasks");
+loadDefaultContent();
+
+const newCategoryButton = document.getElementById("button-new-category");
+const newTaskButton = document.getElementById("button-new-task");
+const closeModalButtons = document.querySelectorAll(".close-button");
+
+const newCategoryForm = document.getElementById("form-new-category");
+const newTaskForm = document.getElementById("form-new-task");
+
+const allTasksButton = document.getElementById("all-tasks");
+const importantButton = document.getElementById("important");
+
+newCategoryButton.addEventListener("click", displayNewCategoryModal);
+newTaskButton.addEventListener("click", displayNewTaskModal);
+
+closeModalButtons.forEach(button => {
+    button.addEventListener("click", closeModals);
+});
+
+newCategoryForm.addEventListener("submit", submitNewCategory);
+newTaskForm.addEventListener("submit", submitNewTask);
+
+allTasksButton.addEventListener("click", () => displayContent("All tasks"));
+importantButton.addEventListener("click", () => displayC("Important"));
